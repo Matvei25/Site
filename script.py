c21ff75f1dcda86153bfdcbc2e5477f33773aa9b
Файри BFDI <133587107+Matvei25@users.noreply.github.com>
2024-01-31 12:14:07 +0300

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,9 @@
-import browser
-browser.alert("Test")
+import turtle
+from turtle import *
+turtle = Turtle()
+screen = Screen()
+def circle():
+  for i in range(10):
+    turtle.circle(50)
+    turtle.left(36)
+circle()
