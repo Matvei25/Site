28e762371c7d46412cbca46a01a5c4ba032e21cb
Файри BFDI <133587107+Matvei25@users.noreply.github.com>
2024-07-24 15:24:54 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 echo "PHP is working"
+<?php
+$nonsenseArray = [
+    "Летучие свиньи",
+    "Зеленые облака",
+    "Космические бананы",
+    "Танцующие кактусы",
+    "Молоко из радуги"
+];
+
+header('Content-Type: application/json');
+echo json_encode($nonsenseArray);
+?>
