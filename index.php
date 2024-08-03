bce0a986a27c3291c06d69a21f370cff0f12422c
Матвей25 <133587107+Matvei25@users.noreply.github.com>
2024-08-03 19:10:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,1 @@
 echo "PHP is working"
-<?php
-$nonsenseArray = [
-    "Летучие свиньи",
-    "Зеленые облака",
-    "Космические бананы",
-    "Танцующие кактусы",
-    "Молоко из радуги"
-];
-
-header('Content-Type: application/json');
-echo json_encode($nonsenseArray);
-?>
